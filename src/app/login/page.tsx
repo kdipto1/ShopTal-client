a9175396af2888ececc9d314c3434c4e0abd37af
Dipto Karmaker <96812332+kdipto1@users.noreply.github.com>
2024-05-19 20:31:56 +0600

    shadcn ui - setuped

---
+++ src/app/login/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen flex justify-center self-center">
+      <Button>Login</Button>
+    </div>
+  );
+}
