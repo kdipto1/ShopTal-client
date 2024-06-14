c78f5a37fe7f479f9c71d07e80a4a626442734b1
Dipto Karmaker <96812332+kdipto1@users.noreply.github.com>
2024-06-14 14:36:03 +0600

    ADDED: login form added in login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,3 +1,16 @@
+import LoginForm from "@/components/login-page-components/LoginForm";
+import second from "@/app/assets/undraw_access_account_re_8spm.svg";
+import Image from "next/image";
+
 export default function LoginPage() {
-  return <div className="h-screen bg-gray-100"></div>;
+  return (
+    <div className="h-screen bg-gray-100 sm:flex-col-reverse flex justify-around md:flex-row items-center">
+      <div>
+        <Image src={second} alt="login image" width={500} height={500} />
+      </div>
+      <div className="max-w-lg">
+        <LoginForm />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/login-page-components/LoginForm.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../shadcn-ui/form";
+import { Input } from "../shadcn-ui/input";
+import { Button } from "../shadcn-ui/button";
+
+const formSchema = z.object({
+  phone: z.string().min(10, {
+    message: "Username must be at least 2 characters.",
+  }),
+  password: z.string().min(6, {
+    message: "Password must be at least 2 characters.",
+  }),
+});
+
+const LoginForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      phone: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+    const phone = `${values.phone.slice(0, 3)}-${values.phone.slice(
+      3,
+      6
+    )}-${values.phone.slice(6, 10)}`;
+    const password = values.password;
+    const response = await fetch("http://localhost:5000/api/v1/auth/login", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ phone, password }),
+    });
+    const data = await response.json();
+    console.log(data);
+  };
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="phone"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Phone</FormLabel>
+              <FormControl>
+                <Input type="tel" placeholder="1234567890" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                  This is your public display name.
+                </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input type="password" placeholder="shadcn" {...field} />
+              </FormControl>
+              {/* <FormDescription>
+                  This is your public display name.
+                </FormDescription> */}
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  );
+};
+
+export default LoginForm;
