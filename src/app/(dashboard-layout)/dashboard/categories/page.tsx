2050b6d9d67b56b61be8a236116606bcbae8d35f
Dipto Karmaker <96812332+kdipto1@users.noreply.github.com>
2024-10-01 21:44:17 +0600

    ADDED: data table for showing catrgory list added in dashboard category page added

--- src/app/(dashboard-layout)/dashboard/categories/page.tsx
+++ src/app/(dashboard-layout)/dashboard/categories/page.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import ContentLayout from "@/components/dashboard-page-components/ContentLayout";
+import { DataTable } from "@/components/dashboard-page-components/data-table";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -8,9 +11,101 @@ import {
   BreadcrumbSeparator,
 } from "@/components/shadcn-ui/breadcrumb";
 import { Button } from "@/components/shadcn-ui/button";
+import { ColumnDef } from "@tanstack/react-table";
+import { ChevronRightIcon } from "lucide-react";
 import Link from "next/link";
+import { useCallback, useState } from "react";
+
+export type Category = {
+  id: string;
+  name: string;
+};
 
 const CategoriesPage = () => {
+  const [data, setData] = useState<Category[]>([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const columns: ColumnDef<Category>[] = [
+    {
+      accessorKey: "id",
+      header: "ID",
+      cell: ({ row }) => (
+        <div className="font-medium">{row.getValue("id")}</div>
+      ),
+    },
+    {
+      accessorKey: "name",
+      header: "Name",
+      cell: ({ row }) => (
+        <div className="font-medium">{row.getValue("name")}</div>
+      ),
+    },
+    {
+      header: "Actions",
+      id: "actions",
+      cell: ({ row }) => {
+        const product = row.original;
+        return (
+          <div className="">
+            <Button variant="ghost" size="sm" asChild>
+              <Link href={`/dashboard/categories/${product.id}`}>
+                Edit
+                <ChevronRightIcon className="ml-2 h-4 w-4" />
+              </Link>
+            </Button>
+          </div>
+        );
+      },
+    },
+  ];
+  const fetchData = useCallback(
+    async ({
+      pageIndex,
+      pageSize,
+      searchTerm,
+      sorting,
+    }: {
+      pageIndex: number;
+      pageSize: number;
+      searchTerm: string;
+      sorting: any;
+    }) => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(
+          `http://localhost:5000/api/v1/categories?page=${
+            pageIndex + 1
+          }&limit=${pageSize}&searchTerm=${searchTerm}&sort=${JSON.stringify(
+            sorting
+          )}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch categories");
+        }
+
+        const result = await response.json();
+        const totalItems = result.data.meta.total;
+        const itemsPerPage = result.data.meta.limit;
+        const calculatedTotalPages = Math.ceil(totalItems / itemsPerPage);
+
+        setData(result.data.data);
+        setTotalPages(calculatedTotalPages);
+      } catch (error) {
+        console.error("Error fetching categories:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    []
+  );
   return (
     <ContentLayout title="Categories">
       <Breadcrumb>
@@ -36,6 +131,15 @@ const CategoriesPage = () => {
       <Link href="/dashboard/categories/new">
         <Button>Add New Category</Button>
       </Link>
+      <div className="mt-2">
+        <DataTable
+          columns={columns}
+          data={data}
+          fetchData={fetchData}
+          totalPages={totalPages}
+          isLoading={isLoading}
+        />
+      </div>
     </ContentLayout>
   );
 };
