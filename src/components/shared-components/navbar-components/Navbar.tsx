2216caab4a67790448b92df0e310193a9858801a
Dipto Karmaker <96812332+kdipto1@users.noreply.github.com>
2024-11-27 18:51:17 +0600

    REMOVED: unnecessary codes removed

--- src/components/shared-components/navbar-components/Navbar.tsx
+++ src/components/shared-components/navbar-components/Navbar.tsx
@@ -1,29 +1,3 @@
-// "use client";
-// import { NavigationMenuDemo } from "./NavbarMenu";
-// import { usePathname } from "next/navigation";
-
-// const Navbar = () => {
-//   const pathname = usePathname();
-
-//   const hideNavbarRoutes = ["/login", "/signup"];
-
-//   const isDashboardRoute = pathname.startsWith("/dashboard");
-
-//   const isHiddenRoute = hideNavbarRoutes.includes(pathname);
-
-//   const shouldHideNavbar = isDashboardRoute || isHiddenRoute;
-
-//   return !shouldHideNavbar ? (
-//     <div className="flex flex-wrap justify-center">
-//       <NavigationMenuDemo />
-//     </div>
-//   ) : (
-//     <></>
-//   );
-// };
-
-// export default Navbar;
-
 "use client";
 
 import { usePathname } from "next/navigation";
