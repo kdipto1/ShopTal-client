3fc017058f0a05b43f107ee0e572288ab8f8b59b
Dipto Karmaker <96812332+kdipto1@users.noreply.github.com>
2024-11-27 19:58:14 +0600

    ADDED: dashboard - edit subcategory page added

---
+++ src/app/(dashboard-layout)/dashboard/subcategories/edit/[id]/page.tsx
@@ -0,0 +1,51 @@
+import ContentLayout from "@/components/dashboard-page-components/ContentLayout";
+import CreateSubcategoryForm from "@/components/dashboard-page-components/subcategories-page-components/new-page-components/CreateSubcategoryForm";
+import SubcategoryForm from "@/components/dashboard-page-components/subcategories-page-components/SubcategoryForm";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/shadcn-ui/breadcrumb";
+import Link from "next/link";
+
+export default async function EditSubcategoryPage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  return (
+    <ContentLayout title="New">
+      <Breadcrumb>
+        <BreadcrumbList>
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/">Home</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/dashboard">Dashboard</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbLink asChild>
+              <Link href="/dashboard/subcategories">Subcategories</Link>
+            </BreadcrumbLink>
+          </BreadcrumbItem>
+          <BreadcrumbSeparator />
+          <BreadcrumbItem>
+            <BreadcrumbPage>Edit</BreadcrumbPage>
+          </BreadcrumbItem>
+        </BreadcrumbList>
+      </Breadcrumb>
+      {/* <PlaceholderContent /> */}
+      {/* <CreateSubcategoryForm /> */}
+      <SubcategoryForm subcategoryId={params.id} />
+    </ContentLayout>
+  );
+}
