f0f4e19ceb462a1992352436dcb45c120c4fb3a4
Dipto Karmaker <96812332+kdipto1@users.noreply.github.com>
2024-12-22 14:40:43 +0600

    FIXED: navbar responsive issue with tablet/laptop screens are fixed

--- src/components/shared-components/navbar-components/Header.tsx
+++ src/components/shared-components/navbar-components/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ShoppingCart, User, LogIn } from "lucide-react";
+import { LogIn, ShoppingCart, User } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import SearchProducts from "./HeaderSearchProducts";
